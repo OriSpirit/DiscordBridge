86014586b7a863accafdca37fb6e4b0ad3f2fc1b
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-07-22 21:17:45 +0800

    IT WORKS ALL THE BUGS WERE AT COMPILE TIME

--- src/main/java/com/spiritlight/discordchat/api/DiscordInstance.java
+++ src/main/java/com/spiritlight/discordchat/api/DiscordInstance.java
@@ -4,18 +4,22 @@ import com.spiritlight.discordchat.Main;
 import com.spiritlight.discordchat.events.DiscordEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.AllowedMentions;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.security.auth.login.LoginException;
+import java.util.EnumSet;
 
 public class DiscordInstance {
     private static JDA instance;
     private static final Logger logger = LogManager.getLogger(Main.MODID);
     private static State currentState = State.OFFLINE;
+    private static final EnumSet<Message.MentionType> deny = EnumSet.of(Message.MentionType.EVERYONE, Message.MentionType.HERE, Message.MentionType.ROLE);
 
     public static void start() {
         if(currentState != State.OFFLINE) {
@@ -30,6 +34,7 @@ public class DiscordInstance {
             builder.addEventListeners(new DiscordEvent());
             instance = builder.build();
             instance.awaitReady();
+            AllowedMentions.setDefaultMentions(EnumSet.complementOf(deny));
             currentState = State.ONLINE;
         } catch (LoginException e) {
             logger.log(Level.ERROR, "Unable to login to the discord instance!");
